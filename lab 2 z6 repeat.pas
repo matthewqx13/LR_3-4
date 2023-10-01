896c8f9633eb03b50e1a6bb9119ad8c51a3830c9
matthewqx13 <144227151+matthewqx13@users.noreply.github.com>
2023-10-01 18:23:01 +0300

    Update lab 2 z6 repeat.pas

--- lab 2 z6 repeat.pas
+++ lab 2 z6 repeat.pas
@@ -7,11 +7,12 @@ var
     readln(num);
     
     count:=0;
-    
-    for i:= 1 to num do
-    begin
-      if num mod i = 0 then count:=count + 1;
-    end;
+    i:=1;
+
+    repeat
+    if num mod i = 0 then count := count + 1;
+    i := i + 1;
+  until i > num;
     
     writeln('Кол-во делителей числа ', count);
   end.
